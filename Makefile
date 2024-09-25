52779d65388268c0bc08524069ff903fa8d2b257
Gabe <Gabriel.d.owen-1@ou.edu>
2024-09-25 13:20:24 -0500

    add window and game loop

--- Makefile
+++ Makefile
@@ -3,10 +3,9 @@ BUILD_DIR = build/debug
 CC = g++
 SRC_FILES = $(wildcard $(SRC_DIR)/*.cpp)
 OBJ_NAME = play
-INLCUDE+_PATHS = -Iinclude
+INCLUDE_PATHS = -Iinclude
 LIBRARY_PATHS = -Llib
 COMPILER_FLAGS = -std=c++11 -Wall -O0 -g
-LINKER_FLAGS = -lSDL2  
-
+LINKER_FLAGS = -lSDL2  -lSDL2_image 
 all: 
 	$(CC) $(COMPILER_FLAGS) $(LINKER_FLAGS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(SRC_FILES) -o $(BUILD_DIR)/$(OBJ_NAME)
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "RenderWindow.hpp"
 
 using namespace std;
 
+int main(int argv, char* args[]) {
 
-int main() {
-
+    cout << "Testing if libraries can be initialized" << endl;
     if (SDL_Init(SDL_INIT_EVERYTHING) > 0) {
         cout << "SDL_Init failed: " << SDL_GetError() << endl;
         return EXIT_FAILURE;
     }
+    if (!(IMG_Init(IMG_INIT_PNG))) {
+        cout << "SDL_Init failed: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
 
-    cout << "Hello, World!" << endl;
-    cout << "This is a simple C++ program." << endl;
+    RenderWindow window("Hello World", 1280, 720);
 
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    // GAME LOOP
+    while(isRunning){
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                isRunning = false;
+            }
+        }
+    }
+
+
+
+    // clean up
+    window.cleanUp();
+    SDL_Quit();
+
+    cout << "Program executed" << endl;
     return EXIT_SUCCESS;
 }
